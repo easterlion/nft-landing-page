64517a29966a7087eaeef71e406208ea26f54d52
Leon Belo <leon.belo@hotmail.com>
2022-07-23 21:03:57 +0200

    updated text and color

--- js/countdown.js
+++ js/countdown.js
@@ -22,3 +22,10 @@ const countdownfunction = setInterval(function () {
     clockdiv.querySelector(".seconds").innerHTML = seconds;
   }
 }, 1000);
+<iframe
+src="https://gateway.ipfscdn.io/ipfs/QmYQ7F7jT9K8Yyctw99zPKU65a1L4mXktcs24dvVKZDnQW/signature-drop.html?contract=0x67be9C204e3117361a76550DC6fbc0BF2E277822&chainId=137"
+width="600px"
+height="600px"
+style="max-width:100%;"
+frameborder="0"
+></iframe>
